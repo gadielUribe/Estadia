1b3fd6222ab8f201855a0ac51e05a97e6e9c7db6
gadielUribe <vugo222163@upemor.edu.mx>
2025-09-07 23:06:04 -0600

    login

--- EcoGestion/EcoGestion/settings.py
+++ EcoGestion/EcoGestion/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'usuario.apps.UsuarioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- EcoGestion/usuario/views.py
+++ EcoGestion/usuario/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
-# Create your views here.
+from .forms import LoginForm
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, 
+                                username=cd['username'], 
+                                password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse('Usuario Autenticado')
+                else:
+                    return HttpResponse('Cuenta Deshabilitada')
+            else:
+                return HttpResponse('Usuario No Encontrado')
+    else:
+        form = LoginForm()
+    
+    return render(request, 'account/login.html', {'form': form})
