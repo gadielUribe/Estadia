78efec11a95c1f1fcb03797fb6ad42fb762358c7
Ascencio <carlosonofre172@gmail.com>
2025-09-12 19:42:36 -0600

    CRUD Funcional, sin llave foranea

--- EcoGestion/plantas/models.py
+++ EcoGestion/plantas/models.py
@@ -1,26 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+# Modelo para plantas y árboles
 class plantaArbol(models.Model):
     id_planta = models.AutoField(primary_key=True)
     descripcion = models.CharField(verbose_name="Descripción", max_length=600, null=False)
     nombre_cientifico = models.CharField(verbose_name="NombreCientífico", max_length=100, null=False)
-    fecha_plantacion = models.DateTimeField(verbose_name="FechaPlantación", null=True)
+    fecha_plantacion = models.DateField(verbose_name="FechaPlantación", null=True)
     imagen_url= models.CharField(verbose_name="ImagenURL", max_length=300, null=True)
     periodicidad_riego = models.IntegerField(verbose_name="PeriodicidadRiego", null=False)
     periodicidad_poda = models.IntegerField(verbose_name="PeriodicidadPoda", null=False)
     periodicidad_fumigacion = models.IntegerField(verbose_name="PeriodicidadFumigación", null=False)
     latitud = models.FloatField(verbose_name="Latitud", null=False)
     longitud = models.FloatField(verbose_name="Longitud", null=False)
-    id_ubicacion = models.IntegerField(verbose_name="IDUbicación", null=False)
 
+    # Información de la tabla
     class Meta:
         db_table = "PlantaArbol"
         managed = False
 
+    # Representación en cadena del objeto
     def __str__(self):
         texto = "{0} - {1}"
-        return texto.format(self.id, self.nombre_cientifico)
+        return texto.format(self.id_planta, self.nombre_cientifico)
+
+
 
 
 
--- EcoGestion/plantas/views.py
+++ EcoGestion/plantas/views.py
@@ -1,20 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import plantaForm
+from .models import plantaArbol
 
+# Pagina de Incio para CRUD de plantas y árboles
 def inicio(request):
-    return HttpResponse("Hola, Mundo. Estás en la aplicación de Plantas.")
+    plantas = plantaArbol.objects.all().order_by('id_planta')
+    return render(request, 'arboles/read.html', {'plantas': plantas})
 
+# Crear un nuevo árbol o planta
 def crear(request):
-    form = plantaForm()
-    if request.method == 'GET': 
-        return render(request, 'arboles/create.html',
-            {
-                'form': plantaForm
-            })
+    if request.method == 'POST': 
+        form = plantaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('crear')
     else:
-        print(request.POST)
-        return render(request, 'arboles/create.html',
-            {
-                'form': plantaForm
-            })
+        form = plantaForm()
+
+    return render(request, 'arboles/create.html', {'form': form})
+
+# Editar un árbol o planta existente
+def editar(request, id_arbol):
+    arbol = get_object_or_404(plantaArbol, pk=id_arbol)
+    if request.method == 'POST':
+        form = plantaForm(request.POST, instance=arbol)
+        if form.is_valid():
+            form.save() 
+            return redirect('inicio')
+    else:
+        form = plantaForm(instance=arbol)
+    return render(request, 'arboles/update.html', {'form':form, 'arbol':arbol})
+
+# Eliminar un árbol o planta existente
+def eliminar(request, id_arbol):
+    arbol = get_object_or_404(plantaArbol, pk=id_arbol)
+    if request.method == 'POST':
+        arbol.delete()
+        return redirect('inicio')
+        
+    return render(request, 'arboles/delete.html', {'arbol':arbol})
+    
