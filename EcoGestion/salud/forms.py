75ae2b3401e786556da1e15253962c5f47d98cd5
Gerardo Ascencio <148816508+Ascens12@users.noreply.github.com>
2025-11-21 22:11:51 -0600

    Correccion Salud

--- EcoGestion/salud/forms.py
+++ EcoGestion/salud/forms.py
@@ -10,4 +10,28 @@ class SaludForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['planta'].queryset = plantaArbol.objects.filter(registro_salud__isnull=True)
+
+        qs = plantaArbol.objects.filter(registro_salud__isnull=True)
+
+        if self.instance and self.instance.pk and self.instance.planta_id:
+            qs = qs | plantaArbol.objects.filter(pk=self.instance.planta_id)
+
+        self.fields['planta'].queryset = qs.order_by('nombre_comun')
+
+    def clean(self):
+        cleaned = super().clean()
+        planta = cleaned.get('planta')
+        if not planta:
+            return cleaned
+
+        if not self.instance or not self.instance.pk:
+            # Crear
+            if SaludRegistro.objects.filter(planta=planta).exists():
+                self.add_error('planta', 'Esta planta ya tiene un registro de salud.')
+        else:
+            # Editar
+            if (planta.pk != self.instance.planta_id and
+                SaludRegistro.objects.filter(planta=planta).exists()):
+                self.add_error('planta', 'La planta seleccionada ya tiene un registro de salud.')
+
+        return cleaned
